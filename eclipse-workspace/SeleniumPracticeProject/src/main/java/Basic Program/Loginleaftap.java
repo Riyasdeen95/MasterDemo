f1106b6f5ec6b35938a77457807d69243fb6f4df
Riyasdeen40 <riyaskansa@gmail.com>
2023-12-18 17:29:12 +0530

    Basic code

---
+++ eclipse-workspace/SeleniumPracticeProject/src/main/java/Basic Program/Loginleaftap.java
@@ -0,0 +1,31 @@
+package practice1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Loginleaftap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// setup 
+		WebDriverManager.chromedriver().setup();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\RIYASDEEN\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		//Launch the browser
+		ChromeDriver driver = new ChromeDriver();
+
+		//Get URL
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		//Maximize the Window
+		driver.manage().window().maximize();
+
+		//Get Title
+		String title = driver.getTitle();
+		System.out.println(title);
+
+
+	}
+
+}
---
+++ eclipse-workspace/SeleniumPracticeProject/src/main/java/Basic Program/SetWebdriver.java
@@ -0,0 +1,15 @@
+package practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SetWebdriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//set driver
+		WebDriverManager.chromedriver().setup();
+
+	}
+
+}
